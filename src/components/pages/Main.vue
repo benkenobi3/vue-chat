f0e41cec89c7059606ffd87c84252b3e383a48b6
varvarakisel <barbara05@mail.ru>
2021-01-19 00:36:57 +0300

    modifying main page

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,36 +1,68 @@
 <template>
-  <div>
-
-    <b-container>
-
+  <div class ="main">
+    <b-container class="welcome-card"> 
       <b-row class="justify-content-center">
         
-        <b-col cols="12">
+        <b-col sm="4">
           
-          
-        <b-button>Начать чат!</b-button>
+          <b-img class="logo" center fluid :src="require('../custom/Group_34.png')"></b-img>
+
+          <b-card class="card" title="poly chat">
+
+              <b-card-text>
+                <b-col>
+                  <b-form-input 
+                  v-model="name" 
+                  size="sm"
+                  placeholder="Введите имя или оставьте поле пустым."
+                  >
+                </b-form-input>
+                </b-col>
+                
+                
+              </b-card-text>
+              <b-card-text>
+                Пользователей онлайн: 
+                <h6 class="amount-of-active-users">112</h6> 
+              </b-card-text>
+
+              <b-button href="#" variant="success">Начать чат!</b-button>
+
+            </b-card>
         </b-col>
 
       </b-row>
-
     </b-container>
-
+    
   </div>
 </template>
 
 <script>
 export default {
-
+ 
 }
 </script>
 
 <style>
-  
-  div {
+
+  .card {
+    background-color: #FAFAFA;
+  }
+
+  .logo {
+    width: 10vw;
+    /* padding-bottom: 5vh; */
+  }
+
+  .welcome-card {
+    align-items: center;
+    padding-top: 28vh;
+    filter: drop-shadow(0.5vw 0.5vh 0.5rem #EBEBEB);
+  }
+
+  .main {
     background: linear-gradient(#E0FFF1 50%, #ffffff 50%);
     height: 100vh;
-    margin-top: 0;
-    padding-top: 0;
   }
-  
+
 </style>
