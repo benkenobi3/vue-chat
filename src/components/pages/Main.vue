fe3ce6232b52810e257c9a21643a3a63298e43f7
varvarakisel <barbara05@mail.ru>
2021-01-18 13:37:32 +0300

    router.js

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+    <h1>Hello Navalny</h1>
   </div>
 </template>
 
@@ -11,5 +11,5 @@ export default {
 </script>
 
 <style>
-
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
+// import store from './store'
+import router from './components/elements/router'
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+
+Vue.prototype.$http = Axios
 
 new Vue({
+
+  // store, может пригодиться
+  router,
+
   render: h => h(App),
+
 }).$mount('#app')
