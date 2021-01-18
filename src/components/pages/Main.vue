d1e95fbae38b0b2fd713182a4409537924820f3c
varvarakisel <barbara05@mail.ru>
2021-01-18 14:53:56 +0300

    main page gradient

--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <h1>Hello Navalny</h1>
+
+    <b-container>
+
+      <b-row class="justify-content-center">
+        
+        <b-col cols="12">
+          <h1>Hello Navalny</h1>
+        <b-button>Начать чат!</b-button>
+        </b-col>
+
+      </b-row>
+
+    </b-container>
+
   </div>
 </template>
 
@@ -11,5 +24,12 @@ export default {
 </script>
 
 <style>
+  
+  div {
+    background: linear-gradient(#E0FFF1 50%, #ffffff 50%);
+    height: 100vh;
+    margin-top: 0;
+    padding-top: 0;
+  }
   
 </style>
