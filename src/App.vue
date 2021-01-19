4f2f6baa14c4885557bc12d610f70368325bbf0e
Max <32428314+benkenobi3@users.noreply.github.com>
2021-01-19 21:24:25 +0300

    fix fonts

--- src/App.vue
+++ src/App.vue
@@ -22,28 +22,45 @@ export default {
     color: #2c3e50;
   }
 
-  @font-face {
-    font-family: "Gilroy";
-    src: local("gilroy-medium"),
-      url(/fonts/gilroy-medium.ttf) format("truetype");
-    font-style: normal;
-    font-weight: 200;
+  .vh-100 {
+    height: 100vh;
   }
+
+
   @font-face {
     font-family: "Gilroy";
     src: local('gilroy-regular'),
-      url(/fonts/gilroy-regular.ttf) format("truetype");
-      font-weight: 100;
+      url(./assets/fonts/gilroy-regular.ttf) format("truetype");
+    font-weight: 200;
   }
+
+  @font-face {
+    font-family: "Gilroy";
+    src: local("gilroy-medium"),
+      url(./assets/fonts/gilroy-medium.ttf) format("truetype");
+    font-style: normal;
+    font-weight: 400;
+  }
+
   @font-face {
     font-family: "Gilroy";
     src: local("gilroy-semibold"),
-      url(/fonts/gilroy-semibold.ttf) format("truetype");
+      url(./assets/fonts/gilroy-semibold.ttf) format("truetype");
+    font-weight: 500;
+  }
+
+  @font-face {
+    font-family: "Gilroy";
+    src: local("gilroy-bold"),
+      url(./assets/fonts/gilroy-bold.ttf) format("truetype");
     font-weight: 600;
   }
 
-  .vh-100 {
-    height: 100vh;
+  @font-face {
+    font-family: "Gilroy";
+    src: local("gilroy-extrabold"),
+      url(./assets/fonts/gilroy-extrabold.ttf) format("truetype");
+    font-weight: 700;
   }
 
 </style>
--- src/components/pages/Main.vue
+++ src/components/pages/Main.vue
@@ -8,21 +8,19 @@
 
           <b-card class="welcome-card mx-auto" title="poly chat">
 
-              <b-card-text>
-                <b-col>
-                  <b-form-input 
-                  v-model="name" 
-                  size="sm"
-                  placeholder="Введите имя">
-                  </b-form-input>
-                </b-col>               
-              </b-card-text>
-              <b-card-text>
-                Пользователей онлайн:
-                <h6 class="amount-of-active-users">112</h6> 
-              </b-card-text>
+            <b-card-text>
+                <b-form-input 
+                v-model="name" 
+                size="sm"
+                placeholder="Введите имя">
+                </b-form-input>             
+            </b-card-text>
 
-              <b-button href="/chat" variant="outline-success">Начать чат!</b-button>
+            <b-button href="/chat" variant="outline-success">Начать чат!</b-button>
+
+            <b-card-text class="mt-2">
+              Пользователей онлайн: <h6 class="amount-of-active-users">112</h6> 
+            </b-card-text>
 
           </b-card>
 
@@ -39,16 +37,16 @@ export default {
 </script>
 
 <style>
+
   .main-page {
     background: linear-gradient(#E0FFF1, #DDEFC6);
-    font-family: Gilroy;
-    font-weight: 600;
   }
 
   .amount-of-active-users {
     color: #2ECC71;
     font-family: "Gilroy";
     font-weight: 200;
+    display: inline;
   }
 
   .logo {
@@ -58,9 +56,19 @@ export default {
   .welcome-card {
     margin-top: 2vh;
     width: 22vw;
-    filter: drop-shadow(0.5vw 0.5vh 0.5rem #EBEBEB);
+    filter: drop-shadow(0.5vw 0.5vh 0.5rem rgba(0,0,0, 0.08));
     font-family: "Gilroy";
-    font-weight: 100;
+    font-weight: 200;
+  }
+
+  .welcome-card h4 {
+    margin-top: 1vh;
+    margin-bottom: 3vh;
+
+    font-family: "Gilroy";
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 30px;
   }
 
 </style>
