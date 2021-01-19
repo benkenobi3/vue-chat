4db1ad901e5ece3cd7f83677ced22710542ad9e6
Alsu1311 <56224152+Alsu1311@users.noreply.github.com>
2021-01-19 20:36:35 +0300

    Chat Designed

--- src/components/pages/Chat.vue
+++ src/components/pages/Chat.vue
@@ -1,19 +1,13 @@
 <template>
-  <div>
+   <div>
+    <b-container fluid class ="main-page ">
+      <b-container class="upperleft-box">
+      <div class= "row no-gutters">
+        <h2 class="logo-text-chat">poly chat</h2>
 
-    <b-container class="chat-page">
-
-      <b-row class="justify-content-center">
-        
-        <b-col cols="12">
-          
-        <b-button>Начать!</b-button>
-        </b-col>
-
-      </b-row>
-
+      </div>
+      </b-container>
     </b-container>
-
   </div>
 </template>
 
@@ -24,10 +18,43 @@ export default {
 </script>
 
 <style>
- .chat-page {
+ .main-page {
     background: linear-gradient(#E0FFF1 50%, #ffffff 50%);
-    height: 100vh;
-    margin-top: 0;
-    padding-top: 0;
+  }
+.logo-text-chat{
+  font-family: Gilroy;
+font-size: 14px;
+font-style: normal;
+font-weight: 700;
+line-height: 17px;
+letter-spacing: 0em;
+text-align: left;
+
+}
+
+ .upperleft-box{
+margin: 60px auto;
+background: #fff;
+padding:0;
+border-radius:7px;
+}
+.col-md-1{
+  background: #eee;
+}
+
+.profile-image{
+  width:50px;
+  height:50px;
+  border-radius:25px;
+}
+
+  .logo {
+    width: 10vw;
+  }
+
+  .welcome-card {
+    margin-top: 2vh;
+    width: 22vw;
+    filter: drop-shadow(0.5vw 0.5vh 0.5rem #EBEBEB);
   }
 </style>
